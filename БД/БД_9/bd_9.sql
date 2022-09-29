08157338dfedb68cc44001e1f3ca4d9bcab83022
shilov <shilov.a.a.1986@gmail.com>
2022-09-30 01:32:29 +0700

    переделал создание таблиц бд_9

--- БД/БД_9/bd_9.sql
+++ БД/БД_9/bd_9.sql
@@ -14,18 +14,25 @@ COMMENT ON COLUMN "ROLES"."id" IS
 
 COMMENT ON COLUMN "ROLES"."role" IS
     'Роль клиента (например: обычный клиент или сотрудник билиотеки)';
+-----------------------------------------------------------------------------------
 
 /* Таблица "возрастное ограничение" */
-CREATE TABLE reader_rating 
-(
-    "id"     NUMBER               GENERATED AS IDENTITY,
-    reader_rating               NUMBER          NOT NULL
-                                                UNIQUE,
-CONSTRAINT pk_reader_rating      PRIMARY KEY ("id")                                              
+CREATE TABLE reader_rating (
+    "id"          NUMBER
+        GENERATED AS IDENTITY,
+    reader_rating NUMBER NOT NULL UNIQUE,
+    CONSTRAINT pk_reader_rating PRIMARY KEY ( "id" )
 );
-COMMENT ON TABLE reader_rating IS 'Таблица рейтинг читателя';
-COMMENT ON COLUMN reader_rating."id" IS 'Идентификационный номер';
-COMMENT ON COLUMN reader_rating.reader_rating IS 'Рейтинг читателя';
+
+COMMENT ON TABLE reader_rating IS
+    'Таблица рейтинг читателя';
+
+COMMENT ON COLUMN reader_rating."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN reader_rating.reader_rating IS
+    'Рейтинг читателя';
+-----------------------------------------------------------------------------------
 
 /* Таблица "читательский билет" */
 CREATE TABLE library_card (
@@ -79,6 +86,7 @@ COMMENT ON COLUMN library_card.id_role IS
 
 COMMENT ON COLUMN library_card.closing_date IS
     'Дата закрытия читательского билета';
+-----------------------------------------------------------------------------------
 
 /*Таблица "теги"*/
 CREATE TABLE tags (
@@ -121,178 +129,320 @@ COMMENT ON COLUMN author.author_firstname IS
 
 COMMENT ON COLUMN author.author_patronymic IS
     'Отчество автора';
-------------------------------------------------------------
 
+    create index index_author 
+on author(author_lastname);
+-----------------------------------------------------------------------------------
 
-
-
-
-
-
+/*Таблица "publishing_house"*/
 CREATE TABLE publishing_house 
 (
-    "id"                        NUMBER              PRIMARY KEY,
+    "id"              NUMBER
+        GENERATED AS IDENTITY,
     publishing_house            VARCHAR2(60 CHAR)   NOT NULL
-                                                    UNIQUE
+                                                    UNIQUE,
+          CONSTRAINT publishing_house PRIMARY KEY ( "id" )                                              
 );
-CREATE SEQUENCE publishing_house_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE TABLE service_category /*абонемент/читалььный зал*/
+COMMENT ON TABLE publishing_house IS
+    'Таблица тздательство';
+
+COMMENT ON COLUMN publishing_house."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN publishing_house.publishing_house IS
+    'Издательство';
+-----------------------------------------------------------------------------------
+
+/*Таблица "service_category" (абонемент/читальный зал)*/
+CREATE TABLE service_category 
 (
-    "id"                        NUMBER              PRIMARY KEY,
+    "id"              NUMBER
+        GENERATED AS IDENTITY,
     service_category            VARCHAR2(60 CHAR)   NOT NULL
-                                                    UNIQUE
+                                                    UNIQUE,
+             CONSTRAINT service_category PRIMARY KEY ( "id" )                                                 
 );
-CREATE SEQUENCE service_category_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE TABLE age_limit /*возрастное ограничение*/
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    age_limit                   VARCHAR2(60 CHAR)   NOT NULL
-                                                    UNIQUE
+COMMENT ON TABLE service_category IS
+    'Таблица сервисная категория (абонемент/читальный зал)';
+
+COMMENT ON COLUMN service_category."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN service_category.service_category IS
+    'Сервисная категория';
+-----------------------------------------------------------------------------------
+
+/*Таблица "возрастное ограничение"*/
+CREATE TABLE age_limit (
+    "id"      NUMBER
+        GENERATED AS IDENTITY,
+    age_limit VARCHAR2(60 CHAR) NOT NULL UNIQUE,
+    CONSTRAINT age_limit PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE age_limit_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE TABLE genres
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    genre                      VARCHAR2(60 CHAR)    NOT NULL
-                                                    UNIQUE
+COMMENT ON TABLE age_limit IS
+    'Таблица сервисная категория (абонемент/читальный зал)';
+
+COMMENT ON COLUMN age_limit."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN age_limit.age_limit IS
+    'Сервисная категория';
+-----------------------------------------------------------------------------------
+
+/*Таблица "жанры"*/
+CREATE TABLE genres (
+    "id"  NUMBER
+        GENERATED AS IDENTITY,
+    genre VARCHAR2(60 CHAR) NOT NULL UNIQUE,
+    CONSTRAINT genres PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE genres_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE TABLE book_type /*книга/журнал*/
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    book_type                   VARCHAR2(60 CHAR)   NOT NULL
-                                                    UNIQUE    
+COMMENT ON TABLE genres IS
+    'Таблица жанры';
+
+COMMENT ON COLUMN genres."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN genres.genre IS
+    'Жанры';
+-----------------------------------------------------------------------------------
+
+/*Таблица "Тип книги" (Книга, журнал, газета)*/
+CREATE TABLE book_type (
+    "id"      NUMBER
+        GENERATED AS IDENTITY,
+    book_type VARCHAR2(60 CHAR) NOT NULL UNIQUE,
+    CONSTRAINT book_type PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE book_type_seq
-START WITH 1
-INCREMENT BY 1;
 
+COMMENT ON TABLE book_type IS
+    'Таблица тип книги (Книга, журнал, газета)';
 
-CREATE TABLE books
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    name_book                   VARCHAR2(100 CHAR)  NOT NULL,
-    tom                         NUMBER,
-    amount/*количество*/        NUMBER,
-    id_book_type                NUMBER              NOT NULL
-        CONSTRAINT b_id_book_type_fk     
-        REFERENCES book_type("id"), 
-    id_age_limit                NUMBER              NOT NULL
-        CONSTRAINT b_id_age_limit_fk     
-        REFERENCES age_limit("id"), 
-    id_publishing_house         NUMBER              NOT NULL
-        CONSTRAINT b_id_publishing_house_fk     
-        REFERENCES publishing_house("id"), 
-    year_of_publishing          NUMBER,
-    price                       NUMBER
+COMMENT ON COLUMN book_type."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN book_type.book_type IS
+    'Тип книги (Книга, журнал, газета)';
+-----------------------------------------------------------------------------------
+
+/*Таблица "Книги" */
+CREATE TABLE books (
+    "id"                NUMBER
+        GENERATED AS IDENTITY,
+    name_book           VARCHAR2(100 CHAR) NOT NULL,
+    tom                 NUMBER,
+    id_book_type        NUMBER NOT NULL
+        CONSTRAINT b_id_book_type_fk
+            REFERENCES book_type ( "id" ),
+    id_age_limit        NUMBER NOT NULL
+        CONSTRAINT b_id_age_limit_fk
+            REFERENCES age_limit ( "id" ),
+    id_publishing_house NUMBER NOT NULL
+        CONSTRAINT b_id_publishing_house_fk
+            REFERENCES publishing_house ( "id" ),
+    year_of_publishing  DATE,
+    price               NUMBER,
+    CONSTRAINT books PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE books_seq
-START WITH 1
-INCREMENT BY 1;
 
+COMMENT ON TABLE books IS
+    'Таблица книги';
 
-CREATE TABLE author_book /*переходная таблица*/
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    id_author                   NUMBER              NOT NULL
-        CONSTRAINT ab_id_author_fk     
-        REFERENCES author("id"), 
-    id_book                     NUMBER              NOT NULL
-        CONSTRAINT ab_id_book_fk     
-        REFERENCES books("id")
+COMMENT ON COLUMN books."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN books.name_book IS
+    'Название книги';
+
+COMMENT ON COLUMN books.tom IS
+    'Том книги';
+
+COMMENT ON COLUMN books.id_book_type IS
+    'id типа книги';
+
+COMMENT ON COLUMN books.id_age_limit IS
+    'id возрастного ограничения';
+
+COMMENT ON COLUMN books.id_publishing_house IS
+    'id издательства';
+
+COMMENT ON COLUMN books.year_of_publishing IS
+    'Дата пудликации';
+
+COMMENT ON COLUMN books.price IS
+    'Цена книги';
+
+CREATE INDEX index_book ON
+    books (
+        name_book,
+        year_of_publishing
+    );
+-----------------------------------------------------------------------------------
+
+/*Переходная таблица "author_book"*/
+CREATE TABLE author_book (
+    "id"      NUMBER
+        GENERATED AS IDENTITY,
+    id_book   NUMBER NOT NULL
+        CONSTRAINT ab_id_book_fk
+            REFERENCES books ( "id" ),
+    id_author NUMBER NOT NULL
+        CONSTRAINT ab_id_author_fk
+            REFERENCES author ( "id" ),
+    CONSTRAINT author_book PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE author_book_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE TABLE tags_book /*связующая таблица*/
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    id_tag                      NUMBER              NOT NULL
-        CONSTRAINT tb_id_tag_fk     
-        REFERENCES tags("id"), 
-    id_book                     NUMBER              NOT NULL
+COMMENT ON TABLE author_book IS
+    'Переходная таблица "автор книги"';
+
+COMMENT ON COLUMN author_book."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN author_book.id_book IS
+    'id книги';
+
+COMMENT ON COLUMN author_book.id_author IS
+    'id автрор книги';
+-----------------------------------------------------------------------------------
+
+/*Переходная таблица "tags_book"*/
+CREATE TABLE tags_book (
+    "id"    NUMBER
+        GENERATED AS IDENTITY,
+    id_book NUMBER NOT NULL
         CONSTRAINT tb_id_book_fk
-        REFERENCES books("id")
+            REFERENCES books ( "id" ),
+    id_tag  NUMBER NOT NULL
+        CONSTRAINT tb_id_tag_fk
+            REFERENCES tags ( "id" ),
+    CONSTRAINT tags_book PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE tags_book_seq
-START WITH 1
-INCREMENT BY 1;
 
-CREATE TABLE genre_book /*переходная таблица*/
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    id_book                     NUMBER              NOT NULL
-        CONSTRAINT gb_id_book_fk     
-        REFERENCES books("id"),
-    id_genre                    NUMBER              NOT NULL
-        CONSTRAINT gb_id_genre_fk     
-        REFERENCES genres("id")
+COMMENT ON TABLE tags_book IS
+    'Переходная таблица "тег книги"';
+
+COMMENT ON COLUMN tags_book."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN tags_book.id_book IS
+    'id книги';
+
+COMMENT ON COLUMN tags_book.id_tag IS
+    'id тега книги';
+-----------------------------------------------------------------------------------
+
+/*переходная таблица "genre_book" */
+CREATE TABLE genre_book (
+    "id"     NUMBER
+        GENERATED AS IDENTITY,
+    id_book  NUMBER NOT NULL
+        CONSTRAINT gb_id_book_fk
+            REFERENCES books ( "id" ),
+    id_genre NUMBER NOT NULL
+        CONSTRAINT gb_id_genre_fk
+            REFERENCES genres ( "id" ),
+    CONSTRAINT genre_book PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE genre_book_seq
-START WITH 1
-INCREMENT BY 1;
 
+COMMENT ON TABLE genre_book IS
+    'Переходная таблица "Жанр книги"';
 
-CREATE TABLE inventory_number
-(
-    "id"                        NUMBER              PRIMARY KEY,
-    id_book                     NUMBER              NOT NULL
-        CONSTRAINT in_id_book_fk     
-        REFERENCES books("id"),
-    arrival_book                DATE,/*дата прибытия на склад*/
-    write_off_date_book         DATE/*дата списания книги*/
+COMMENT ON COLUMN genre_book."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN genre_book.id_book IS
+    'id книги';
+
+COMMENT ON COLUMN genre_book.id_genre IS
+    'id жанра книги';
+-----------------------------------------------------------------------------------
+
+/*Таблица "Инвентарный номер" */
+CREATE TABLE inventory_number (
+    "id"                NUMBER
+        GENERATED AS IDENTITY,
+    id_book             NUMBER NOT NULL
+        CONSTRAINT in_id_book_fk
+            REFERENCES books ( "id" ),
+    /*дата прибытия на склад*/
+    arrival_book        DATE,
+    /*дата списания книги*/
+    write_off_date_book DATE,
+    CONSTRAINT inventory_number PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE inventory_number_seq
-START WITH 1
-INCREMENT BY 1;
 
+COMMENT ON TABLE inventory_number IS
+    'Таблица инвентарный номер';
 
-CREATE TABLE issuance_log /*журнаал выдачи*/
-(
-    "id"                        NUMBER          PRIMARY KEY,
-    id_service_category         NUMBER
-        CONSTRAINT il_service_category_fk     
-        REFERENCES service_category("id"),
-    id_inventory_number            NUMBER
-        CONSTRAINT il_inventory_number_fk     
-        REFERENCES inventory_number("id"),
-    id_book                     NUMBER
-        CONSTRAINT il_id_book_fk     
-        REFERENCES books("id"),
-    id_libray_card              NUMBER
-        CONSTRAINT il_libray_card_fk     
-        REFERENCES library_card("id"),
-    date_of_issue_book          DATE           NOT NULL,/*дата выдачи книги*/
-    delivery_date_book          DATE,   /*дата сдачи книги*/
-    fact_date_book              DATE    /*фактическая дата сдачи книги*/
+COMMENT ON COLUMN inventory_number."id" IS
+    'Идентификационный номер';
+
+COMMENT ON COLUMN inventory_number.id_book IS
+    'id книги';
+
+COMMENT ON COLUMN inventory_number.arrival_book IS
+    'дата прибытия книги на склад';
+
+COMMENT ON COLUMN inventory_number.write_off_date_book IS
+    'дата списания книги со склада';
+-----------------------------------------------------------------------------------
+
+/*Таблица "журнаал выдачи"*/
+CREATE TABLE issuance_log (
+    "id"                NUMBER
+        GENERATED AS IDENTITY,
+    id_service_category NUMBER
+        CONSTRAINT il_service_category_fk
+            REFERENCES service_category ( "id" ),
+    id_inventory_number NUMBER
+        CONSTRAINT il_inventory_number_fk
+            REFERENCES inventory_number ( "id" ),
+    id_book             NUMBER
+        CONSTRAINT il_id_book_fk
+            REFERENCES books ( "id" ),
+    id_libray_card      NUMBER
+        CONSTRAINT il_libray_card_fk
+            REFERENCES library_card ( "id" ),
+/*дата выдачи книги*/
+    date_of_issue_book  DATE NOT NULL,
+/*дата сдачи книги*/
+    delivery_date_book  DATE,   
+/*фактическая дата сдачи книги*/
+    fact_date_book      DATE,
+    CONSTRAINT issuance_log PRIMARY KEY ( "id" )
 );
-CREATE SEQUENCE issuance_log_seq
-START WITH 1
-INCREMENT BY 1;
 
----------------------- Индексирование-----------------------
-create index index_is_log
-on issuance_log(date_of_issue_book);
+COMMENT ON TABLE issuance_log IS
+    'Таблица журнал выдачи';
 
-create index index_book 
-on books(name_book, year_of_publishing);
+COMMENT ON COLUMN issuance_log."id" IS
+    'Идентификационный номер';
 
-create index index_author 
-on author(author_lastname);
+COMMENT ON COLUMN issuance_log.id_service_category IS
+    'id сервисной категории (абонемент/читальный зал)';
 
+COMMENT ON COLUMN issuance_log.id_inventory_number IS
+    'id инвентарного номера';
 
+COMMENT ON COLUMN issuance_log.id_book IS
+    'id книги';
 
+COMMENT ON COLUMN issuance_log.id_libray_card IS
+    'id карты читателя';
 
+COMMENT ON COLUMN issuance_log.date_of_issue_book IS
+    'дата выдачи книги';
+
+COMMENT ON COLUMN issuance_log.delivery_date_book IS
+    'дата сдачи книги';
+
+COMMENT ON COLUMN issuance_log.fact_date_book IS
+    'фактическая дата сдачи книги';
+
+CREATE INDEX index_is_log ON
+    issuance_log (
+        date_of_issue_book
+    );
