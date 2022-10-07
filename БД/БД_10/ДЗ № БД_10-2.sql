e3952bdd124ceb28c040dcbd3819fdee40c68d90
Шилов Антон Александрович <shilov.a.a.1986@gmail.com>
2022-10-07 20:17:19 +0700

    бд 10 второй запрос2

--- БД/БД_10/ДЗ № БД_10-2.sql
+++ БД/БД_10/ДЗ № БД_10-2.sql
@@ -7,12 +7,12 @@ DECLARE
     v_publishing_house VARCHAR2(60 CHAR) := 'Просвещение';
     v_year_of_publishing DATE := '01.01.1990';
     v_price NUMBER := 2500;
-    
+    --массив жанров
     type genresarray IS VARRAY(20) OF VARCHAR2(60 CHAR);
     v_genres genresarray := genresarray('Научно популярный', 'Наука');
     type id_genresarray IS VARRAY(20) OF NUMBER;
     v_id_genres id_genresarray := id_genresarray();
-    
+    --массив тегов
     type tagsarray IS VARRAY(20) OF VARCHAR2(60 CHAR);
     v_tags tagsarray := tagsarray('Капитализм', 'Коммунизм', 'Мировое зло');
     type id_tagsarray IS VARRAY(20) OF NUMBER;
@@ -21,10 +21,17 @@ DECLARE
 --    v_author_lastname author.author_lastname%TYPE := 'Маркс';
 --    v_author_firstname author.author_firstname%TYPE := 'Карл';
 --    v_author_patronymic author.author_patronymic%TYPE;
+    --массив авторов
+    type authorsarray IS VARRAY(10) OF VARCHAR2(60 CHAR);
+    type authorsarray2 IS VARRAY(10) OF authorsarray;
+    v_authors authorsarray2 := authorsarray2(authorsarray('Маркс', 'Карл'), authorsarray('Пушкин', 'Александр'));
+    
+    type id_authorsarray IS VARRAY(20) OF NUMBER;
+    v_id_authors id_authorsarray := id_authorsarray();  
+    
     --количество книг
     v_count_book NUMBER := 12;
     v_id_book NUMBER;
-
 
     v_id_book_type NUMBER;
     v_id_age_limit NUMBER;
@@ -148,7 +155,41 @@ BEGIN
         WHERE
             g.genre = v_genres(i);
     END LOOP;
-            
+-------------------------------------------------------
+    --проверка на существование автора
+    FOR i IN 1..v_authors.count LOOP
+        v_id_authors.extend();
+        SELECT
+            COUNT(*)
+        INTO v_id_authors(i)
+        FROM
+            author a
+        WHERE
+            a.author_lastname||a.author_firstname = v_authors(i)(1) || v_authors(i)(2);
+        dbms_output.put_line(v_authors(2)(1));
+    --если нет делаем запись
+       IF v_id_authors(i) = 0 THEN
+       
+            INSERT INTO author (
+                author_lastname,
+                author_firstname,
+                author_patronymic
+            ) VALUES (
+                v_authors(i)(1),
+                v_authors(i)(2),
+                v_authors(i)(3)
+            );
+        
+        END IF;
+        --достаём id
+--        SELECT
+--            a."id"
+--        INTO v_id_authors(i)
+--        FROM
+--            author a
+--        WHERE
+--            a.author_lastname||a.author_firstname = v_authors(i)(1) || v_authors(i)(2);
+    END LOOP;  
 -------------------------------------------------------
     INSERT INTO books (
         NAME_BOOK,
@@ -173,8 +214,6 @@ BEGIN
         INSERT INTO inventory_number ( id_book ) VALUES ( v_id_book );
     
     END LOOP;
-
-
 
 
 END;
