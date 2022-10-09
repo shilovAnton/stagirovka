bd5154ad5f8f0b1da846a51229f238ef1c0f9716
shilov <shilov.a.a.1986@gmail.com>
2022-10-09 15:07:47 +0700

    сделал 3 метод

--- БД/БД_10/ДЗ № БД_10-3.sql
+++ БД/БД_10/ДЗ № БД_10-3.sql
@@ -8,19 +8,13 @@ DECLARE
     v_tom books.tom%TYPE := 1;
     v_publishing_house publishing_house.publishing_house%TYPE := 'Родина';
 
-    -- v_author_firstname author.author_firstname%TYPE;
-    -- v_author_patronymic author.author_patronymic%TYPE;
-    -- v_id_book books."id"%TYPE;
-    -- v_age_limit age_limit.age_limit%TYPE;
-    -- v_genre books.genre%TYPE;
+    --переменная для читателя
+    v_id_reader library_card."id"%TYPE := 11;
 
-    --переме для читателя
-    v_id_reader library_card."id"%TYPE := 10;
-    -- v_readers_lastname library_card.readers_lastname%TYPE;
-    -- v_readers_firstname library_card.readers_firstname%TYPE;
-    -- v_readers_patronymic library_card.readers_patronymic%TYPE;
-    -- v_reader_rating reader_rating.reader_rating%TYPE;
-    -- v_resader_age NUMBER;
+    --переменная есть ли эта книга у читителя на руках
+    book_in_hand NUMBER := 0;
+    --чичло просроченных книг
+    count_book_foul NUMBER := 0;
 
     --создание курсора книг
     CURSOR cursor_book IS
@@ -78,32 +72,81 @@ BEGIN
     LOOP 
         FETCH cursor_reader INTO v_reader;
         EXIT WHEN cursor_reader%NOTFOUND;
-            DBMS_OUTPUT.PUT_LINE('=======================================================');
-            DBMS_OUTPUT.PUT_LINE('Читатель: '||v_reader.readers_lastname||' '||v_reader.readers_firstname||' '||v_reader.readers_patronymic);
-            DBMS_OUTPUT.PUT_LINE('Рейтинг читателя: '||v_reader.reader_rating);
-            DBMS_OUTPUT.PUT_LINE('Возраст читателя: '||v_reader.age_readers);
-            DBMS_OUTPUT.PUT_LINE('Сумма штрафов: '||v_reader.sum_fines);
-            DBMS_OUTPUT.PUT_LINE('Дата создания читательского билета: '||v_reader.date_of_issue_card);
+        DBMS_OUTPUT.PUT_LINE('=======================================================');
+        DBMS_OUTPUT.PUT_LINE('Читатель: '||v_reader.readers_lastname||' '||v_reader.readers_firstname||' '||v_reader.readers_patronymic);
+        DBMS_OUTPUT.PUT_LINE('Рейтинг читателя: '||v_reader.reader_rating);
+        DBMS_OUTPUT.PUT_LINE('Возраст читателя: '||v_reader.age_readers);
+        DBMS_OUTPUT.PUT_LINE('Сумма штрафов: '||v_reader.sum_fines);
+        DBMS_OUTPUT.PUT_LINE('Дата создания читательского билета: '||TO_CHAR(v_reader.date_of_issue_card, 'dd.mm.yyyy'));
     END LOOP;
-        CLOSE cursor_reader;
+    CLOSE cursor_reader;
 -------------------------------------------------------
     OPEN cursor_book;
     LOOP
         FETCH cursor_book INTO v_current_book;
         EXIT WHEN cursor_book%NOTFOUND;
-            DBMS_OUTPUT.PUT_LINE('=======================================================');
-            DBMS_OUTPUT.PUT_LINE('Название книги: '||v_current_book.name_book);
-            DBMS_OUTPUT.PUT_LINE('Автор: '||v_current_book.author);
-            IF v_current_book.tom IS NOT NULL THEN
-                DBMS_OUTPUT.PUT_LINE('Tom: '||v_current_book.tom);
-            END IF;
-            DBMS_OUTPUT.PUT_LINE('Тип: '||v_current_book.book_type);
-            DBMS_OUTPUT.PUT_LINE('Возрастное ограничение: '||v_current_book.age_limit||'+');
-            DBMS_OUTPUT.PUT_LINE('Издательство: '||v_current_book.publishing_house);    
+
+        DBMS_OUTPUT.PUT_LINE('=======================================================');
+        DBMS_OUTPUT.PUT_LINE('Название книги: '||v_current_book.name_book);
+        DBMS_OUTPUT.PUT_LINE('Автор: '||v_current_book.author);
+        IF v_current_book.tom IS NOT NULL THEN
+            DBMS_OUTPUT.PUT_LINE('Tom: '||v_current_book.tom);
+        END IF;
+        DBMS_OUTPUT.PUT_LINE('Тип: '||v_current_book.book_type);
+        DBMS_OUTPUT.PUT_LINE('Возрастное ограничение: '||v_current_book.age_limit||'+');
+        DBMS_OUTPUT.PUT_LINE('Издательство: '||v_current_book.publishing_house);
+        DBMS_OUTPUT.PUT_LINE('Количество: '||v_current_book.amount);
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------------------------');
+        --проверка на возрастное ограничение
+        IF v_reader.age_readers >= v_current_book.age_limit THEN
+            DBMS_OUTPUT.PUT_LINE('Читатель проходит по возрастному ограничению - Ok');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Читатель НЕ проходит по возрастному ограничению - No!!!');
+        END IF;
+        --проверка на рейтинг
+        IF v_reader.reader_rating >= 3 THEN
+            DBMS_OUTPUT.PUT_LINE('Читатель проходит по рейтингу - Ok');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Читатель НЕ проходит по рейтингу - No!!!');
+        END IF;
+        --запрос на то, что эта книга уже у него наруках
+        SELECT
+            COUNT(*)
+        INTO
+            book_in_hand
+        FROM
+            ISSUANCE_LOG il
+        WHERE
+            IL.ID_BOOK = v_current_book."id" and il.ID_LIBRAY_CARD = v_reader."id"
+            and il.DATE_OF_ISSUE_BOOK IS NULL;
+        IF book_in_hand = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('У читателя нет этой книг на руках - Ok');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Эта книга на руках у читателя - No!!!');
+        END IF;
     END LOOP;   
-        CLOSE cursor_book;
+    CLOSE cursor_book;   
 
-
-
-
+        --проверка на наличее просроченных книг у читателя
+        SELECT
+            COUNT(*)
+        INTO
+            count_book_foul
+        FROM
+            issuance_log l
+        WHERE 
+            l."id" = v_reader."id" and
+            --условие по просрочке
+            l.fact_date_book is null and (SYSDATE - l.delivery_date_book) > 1;
+        IF count_book_foul = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('У читателя нет просроченных книг - Ok');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('У читателя есть просроченные книги - No!!!');
+        END IF;
+        --по штрафам
+        IF v_reader.sum_fines = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('У читателя нет штрафов - Ok');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('У читателя есть штрафы - No!!!');
+        END IF;
 END;
